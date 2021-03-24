cdb5bc116097e11593a5924cb4250fa04ba94738
AMS <alonso_mendess@mail.com>
2021-03-24 10:08:50 -0300

    Atualização

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -12,27 +12,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.PathVariable;
-=======
->>>>>>> seguranca
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/usuarios")
-<<<<<<< HEAD
-@CrossOrigin(origins = "*", allowedHeaders = "*")
-=======
 @CrossOrigin("*")
->>>>>>> seguranca
 public class UsuarioController {
 	
 	@Autowired
 	private UsuarioRepository repository;
-
+	
 	@Autowired
 	private UsuarioService usuarioService;
 
@@ -41,51 +36,27 @@ public class UsuarioController {
 		return ResponseEntity.ok(repository.findAll());
 	}
 
-<<<<<<< HEAD
-=======
-	@Autowired
-	private UsuarioRepository repository;
-
-	@Autowired
-	private UsuarioService usuarioService;
-
-	@GetMapping
-	public ResponseEntity<List<Usuario>> GetAll() {
-		return ResponseEntity.ok(repository.findAll());
-	}
-
->>>>>>> seguranca
 	@PostMapping("/logar")
 	public ResponseEntity<UsuarioLogin> Autentication(@RequestBody Optional<UsuarioLogin> user) {
 		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
-<<<<<<< HEAD
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<Usuario> GetById(@PathVariable long id){
-			return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	public ResponseEntity<Usuario> GetById(@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
-	
-	@PostMapping("/cadastrar")
-	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
-		Optional<Usuario> user = usuarioService.CadastrarUsuario(usuario);
-		try {
-				return ResponseEntity.ok(user.get());
-		} catch (Exception e) {
-			return ResponseEntity.badRequest().build();
-		}
-		
-	}
-	
-=======
+
 
 	@PostMapping("/cadastrar")
-	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
+	public ResponseEntity<Optional<Usuario>> Post(@RequestBody Usuario usuario) { // ADIÇÃO DO OPTIONAL
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(usuarioService.CadastrarUsuario(usuario));
-		
 	}
 
->>>>>>> seguranca
+	@PutMapping
+	public ResponseEntity<Usuario> Put(@RequestBody Usuario usuario) {
+		return ResponseEntity.ok(repository.save(usuario));
+	}
+
 }
